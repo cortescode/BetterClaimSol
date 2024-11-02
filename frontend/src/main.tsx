c54310d8c57f135f26b6e28cdd8f95b8f2cd37d7
Pablo Cortés Rodríguez <cortesrodriguezpablo3@gmail.com>
2024-11-02 16:59:27 +0100

    Continue fixing connectivity between backend and frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,30 +9,16 @@ import { PhantomWalletAdapter,SolflareWalletAdapter } from '@solana/wallet-adapt
 import { Buffer } from 'buffer';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
-// import { clusterApiUrl } from '@solana/web3.js';
+import { clusterApiUrl } from '@solana/web3.js';
 window.Buffer = Buffer
 
 const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
 
 
-/* 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ConnectionProvider endpoint={clusterApiUrl('devnet', true)}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <App />
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  </React.StrictMode>
-);
-*/
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ConnectionProvider endpoint="https://solana-mainnet.api.syndica.io/api-key/4eW91Uf1tytzBvzvPuR9jWG3Tpy6AdA2bADJS6vrB4W8EN2y8Ch6k6JiQKgoArNX8zrz7HFeJmGrfHFRzhVZk8Dd41fEJFcPgid">
+    <ConnectionProvider endpoint={clusterApiUrl('mainnet-beta', true)}>
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>
           <App />
@@ -42,18 +28,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   </React.StrictMode>
 );
 
-
-
-
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <ConnectionProvider endpoint={clusterApiUrl('devnet', true)}>
-//       <WalletProvider wallets={wallets} autoConnect>
-//         <WalletModalProvider>
-//           <App />
-//         </WalletModalProvider>
-//       </WalletProvider>
-//     </ConnectionProvider>
-//   </React.StrictMode>
-// );
