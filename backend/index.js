0aec7c0f47b5855db01d7ddb5427ea9b18dd982d
Pablo Cortés Rodríguez <cortesrodriguezpablo3@gmail.com>
2024-11-04 21:03:53 +0100

    Removes cors checking

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,7 @@ app.get('/api/solana-key', (req, res) => {
   // Ideally, add authentication here to protect this route
   res.json({ apiKey: SOLANA_API_KEY });
 });
+/* 
 // Middleware to check the request origin
 app.use((req, res, next) => {
     const allowedOrigins = ['https://betterclaimsol.xyz'];
@@ -56,7 +57,7 @@ app.use((req, res, next) => {
     }
     next();
 });
-
+ */
 // Importing routers
 app.use("/api/affiliation", affiliation_router);
 app.use("/api/claim-transactions", claim_transactions_router);
